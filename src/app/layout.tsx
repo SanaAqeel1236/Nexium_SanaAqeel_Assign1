d25d5acf4852d535134254fb7489fc9304fdf3cb
SanaAqeel1236 <sanaaqeelmalik@gmail.com>
2025-07-07 10:36:08 +0500

    3rd Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      
+      ><ThemeProvider  attribute="class"
+            defaultTheme="light"
+            enableSystem
+            themes={["red","light","dark"]}
+            disableTransitionOnChange>{children}</ThemeProvider>
+        
       </body>
     </html>
   );
